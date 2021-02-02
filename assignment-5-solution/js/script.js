19b78267bd0f71edcde5397f36c48017b1f00a37
hinaJain728 <69239261+hinaJain728@users.noreply.github.com>
2021-02-02 21:03:17 +0530

    Update script.js

--- assignment-5-solution/js/script.js
+++ assignment-5-solution/js/script.js
@@ -102,7 +102,7 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
-	var chosenCategoryShortName = chooseRandomCategory (categories);
+	var chosenCategoryShortName = chooseRandomCategory (categories).short_name;
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -117,12 +117,8 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       // var homeHtmlToInsertIntoMainPage = ....
-	var homeHtmlToInsertIntoMainPage = function (chosenCategoryShortName, randomCategoryShortName, propValue) {
-		var propToReplace = "{{" + randomCategoryShortName + "}}";
-		chosenCategoryShortName = chosenCategoryShortName
-		.replace(new RegExp(propToReplace, "g"), propValue);
-		return string;
-	};
+	chosenCategoryShortName = "'" + chosenCategoryShortName + "'";
+        var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
